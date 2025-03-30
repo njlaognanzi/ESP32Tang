f950e006850ef80b8a4ce02305b519de96c3ae6d
njlaognanzi <njlaoganzi@hotmail.com>
2025-03-30 16:00:41 +0800

    Create: Subfolders for System
    Create: Subfolders for System

---
+++ Document/Tang_Nano_9K/7_toolchain/readme.txt
@@ -0,0 +1,2 @@
+链接：https://eyun.baidu.com/s/3qYZ6lKK 密码：WeQk
+Link:    https://eyun.baidu.com/s/3qYZ6lKK Password:WeQk
---
+++ Software/Verilog/GW1NR_9/src/gowin_rpll/gowin_rpll_tmp.v
@@ -0,0 +1,19 @@
+//Copyright (C)2014-2025 Gowin Semiconductor Corporation.
+//All rights reserved.
+//File Title: Template file for instantiation
+//Tool Version: V1.9.11.01 Education (64-bit)
+//Part Number: GW1NR-LV9QN88PC6/I5
+//Device: GW1NR-9
+//Device Version: C
+//Created Time: Sun Mar 30 15:15:05 2025
+
+//Change the instance name and port connections to the signal names
+//--------Copy here to design--------
+
+    Gowin_rPLL your_instance_name(
+        .clkout(clkout), //output clkout
+        .reset(reset), //input reset
+        .clkin(clkin) //input clkin
+    );
+
+//--------Copy end-------------------
